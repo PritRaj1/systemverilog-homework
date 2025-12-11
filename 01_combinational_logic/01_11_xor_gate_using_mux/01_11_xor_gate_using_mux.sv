786aea87be92770aa25e2909edbe3cfb999690c0
PritRaj1 <prithvi.raj1@outlook.com>
2025-12-10 22:05:08 -0800

    fin comb

--- 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
+++ 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
@@ -27,5 +27,23 @@ module xor_gate_using_mux (
   // Implement xor gate using instance(s) of mux,
   // constants 0 and 1, and wire connections
 
+  // Intermediate
+  logic not_b;
+
+  // ~, but with mux
+  mux not_gate (
+      .d0 (1'b1),
+      .d1 (1'b0),
+      .sel(b),
+      .y  (not_b)
+  );
+
+  mux xor_gate (
+      .d0 (b),
+      .d1 (not_b),
+      .sel(a),
+      .y  (o)
+  );
+
 
 endmodule
